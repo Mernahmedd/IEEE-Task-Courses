87084b2a6f91b53532060a070f783a7608e63c7c
Merna Ahmed <merna0751@gmail.com>
2022-05-13 02:38:28 +0200

    edit on files

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-let popupVideo = document.querySelector(".popup-video");
-popupVideo.onclick = function () {
-  document.querySelector(".popup").classList.add(".popup-video-open");
-};
-
+let x = "";
+document.addEventListener("click", (e) => {
+  x = e.target.getAttribute("src");
+  if (x != null && x != "images/html.png") {
+    if (x.includes("mp4") != true) {
+      x = x.slice(0, x.indexOf(".") + 1);
+      x += "mp4";
+    }
+    video.setAttribute("src", x);
+    document.querySelector(".popup").classList.add("popup-open");
+  }
+});
+let video = document.querySelector(".popup-video");
 let popupSpan = document.querySelector(".popup-span");
-popupSpan.onclick = function () {
-  document.querySelector(".popup").classList.remove(".popup-video-open");
+popupSpan.onclick = function (e) {
+  document.querySelector(".popup").classList.remove("popup-open");
+  video.pause();
 };
